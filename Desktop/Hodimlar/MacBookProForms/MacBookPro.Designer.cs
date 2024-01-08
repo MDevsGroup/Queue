351e58379e3639625c0961865d305dadcf84a17f
Sardoralgoritm <sardorsaminov5@gmail.com>
2024-01-09 00:08:24 +0500

    current

--- Desktop/Hodimlar/MacBookProForms/MacBookPro.Designer.cs
+++ Desktop/Hodimlar/MacBookProForms/MacBookPro.Designer.cs
@@ -48,12 +48,21 @@
             label2 = new Label();
             label1 = new Label();
             guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            label5 = new Label();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            panel6 = new Panel();
+            controlWaiting1 = new MacBookProForms.ControlWaiting();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox1).BeginInit();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
+            panel1.BackColor = SystemColors.ButtonHighlight;
             panel1.Controls.Add(guna2Button3);
             panel1.Controls.Add(guna2Button2);
             panel1.Controls.Add(guna2Button1);
@@ -66,7 +75,7 @@
             panel1.Dock = DockStyle.Left;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(385, 960);
+            panel1.Size = new Size(385, 730);
             panel1.TabIndex = 0;
             // 
             // guna2Button3
@@ -82,7 +91,7 @@
             guna2Button3.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             guna2Button3.ForeColor = Color.White;
             guna2Button3.Image = (Image)resources.GetObject("guna2Button3.Image");
-            guna2Button3.Location = new Point(64, 402);
+            guna2Button3.Location = new Point(64, 287);
             guna2Button3.Margin = new Padding(4);
             guna2Button3.Name = "guna2Button3";
             guna2Button3.ShadowDecoration.CustomizableEdges = customizableEdges2;
@@ -103,7 +112,7 @@
             guna2Button2.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             guna2Button2.ForeColor = Color.White;
             guna2Button2.Image = (Image)resources.GetObject("guna2Button2.Image");
-            guna2Button2.Location = new Point(64, 473);
+            guna2Button2.Location = new Point(64, 358);
             guna2Button2.Margin = new Padding(4);
             guna2Button2.Name = "guna2Button2";
             guna2Button2.ShadowDecoration.CustomizableEdges = customizableEdges4;
@@ -124,7 +133,7 @@
             guna2Button1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             guna2Button1.ForeColor = Color.White;
             guna2Button1.Image = (Image)resources.GetObject("guna2Button1.Image");
-            guna2Button1.Location = new Point(64, 545);
+            guna2Button1.Location = new Point(64, 430);
             guna2Button1.Margin = new Padding(4);
             guna2Button1.Name = "guna2Button1";
             guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges6;
@@ -145,7 +154,7 @@
             SaqlashBtn.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             SaqlashBtn.ForeColor = Color.White;
             SaqlashBtn.Image = (Image)resources.GetObject("SaqlashBtn.Image");
-            SaqlashBtn.Location = new Point(64, 329);
+            SaqlashBtn.Location = new Point(64, 214);
             SaqlashBtn.Margin = new Padding(4);
             SaqlashBtn.Name = "SaqlashBtn";
             SaqlashBtn.ShadowDecoration.CustomizableEdges = customizableEdges8;
@@ -176,9 +185,10 @@
             // 
             // label2
             // 
+            label2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(136, 947);
+            label2.Location = new Point(136, 697);
             label2.Name = "label2";
             label2.Size = new Size(102, 21);
             label2.TabIndex = 2;
@@ -186,9 +196,10 @@
             // 
             // label1
             // 
+            label1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(104, 922);
+            label1.Location = new Point(104, 672);
             label1.Name = "label1";
             label1.Size = new Size(179, 25);
             label1.TabIndex = 1;
@@ -210,11 +221,79 @@
             guna2CirclePictureBox1.TabIndex = 0;
             guna2CirclePictureBox1.TabStop = false;
             // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Symbol", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(68, 17);
+            label5.Name = "label5";
+            label5.Size = new Size(185, 30);
+            label5.TabIndex = 0;
+            label5.Text = "Navbatlar jadvali";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ButtonHighlight;
+            panel2.Controls.Add(label5);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(385, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1030, 60);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Dock = DockStyle.Bottom;
+            panel3.Location = new Point(385, 332);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1030, 398);
+            panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            panel4.Dock = DockStyle.Top;
+            panel4.Location = new Point(385, 60);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(1030, 60);
+            panel4.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            panel5.Dock = DockStyle.Left;
+            panel5.Location = new Point(385, 120);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(46, 212);
+            panel5.TabIndex = 5;
+            // 
+            // panel6
+            // 
+            panel6.Dock = DockStyle.Right;
+            panel6.Location = new Point(1370, 120);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(45, 212);
+            panel6.TabIndex = 6;
+            // 
+            // controlWaiting1
+            // 
+            controlWaiting1.AutoSize = true;
+            controlWaiting1.Dock = DockStyle.Fill;
+            controlWaiting1.Location = new Point(431, 120);
+            controlWaiting1.Name = "controlWaiting1";
+            controlWaiting1.Size = new Size(939, 212);
+            controlWaiting1.TabIndex = 7;
+            // 
             // MacBookPro
             // 
             AutoScaleDimensions = new SizeF(9F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(984, 960);
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(1415, 730);
+            Controls.Add(controlWaiting1);
+            Controls.Add(panel6);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
             Controls.Add(panel1);
             Name = "MacBookPro";
             StartPosition = FormStartPosition.CenterScreen;
@@ -225,7 +304,10 @@
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -240,5 +322,12 @@
         private Guna.UI2.WinForms.Guna2Button SaqlashBtn;
         private Label label2;
         private Label label1;
+        private Label label5;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private Panel panel6;
+        private MacBookProForms.ControlWaiting controlWaiting1;
     }
 }
